4810b4d098d0596a32fbfd3899efc7f6b622cf06
EJ <harrygina09@naver.com>
2025-01-24 15:08:48 +0900

    내주문 페이지

--- src/components/MyOrder/MyOrder.jsx
+++ src/components/MyOrder/MyOrder.jsx
@@ -1,19 +1,31 @@
+import UseData from "../../Hook/UseData";
 import Table from "../Common/Table";
 import "./MyOrder.css";
 
 const MyOrder = () => {
+  const { data: orders, error, isLoading } = UseData("/order");
+  const getProductString = (order) => {
+    const productStringArr = order.products.map(
+      (p) => `${p.product.title}(${p.quantity})`
+    );
+    return productStringArr.join(",");
+  };
   return (
     <section className="align_center myorder_page">
-      <Table headings={["내주문", "상품들", "결재금액", "배송상태"]}>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>iPhone, Power Bank</td>
-            <td>1205,000 원</td>
-            <td>배송중</td>
-          </tr>
-        </tbody>
-      </Table>
+      {orders && (
+        <Table headings={["내주문", "상품들", "결재금액", "배송상태"]}>
+          <tbody>
+            {orders.map((order, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{getProductString(order)}</td>
+                <td>{order.total.toLocaleString("ko-KR")}원</td>
+                <td>{order.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      )}
     </section>
   );
 };
