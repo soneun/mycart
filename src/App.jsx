48773028a9e5cbee754754f37eda305dd45a24f7
EJ <harrygina09@naver.com>
2025-01-24 15:40:10 +0900

    네비게이션 가드

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,7 @@ function App() {
           <Navbar user={user} cartCount={cart.length} />
           <main>
             <ToastContainer position="bottom-right" />
-            <Routing />
+            <Routing user={user} />
           </main>
         </div>
       </CartContext.Provider>
--- src/components/Routing/Routing.jsx
+++ src/components/Routing/Routing.jsx
@@ -6,20 +6,29 @@ import SignupPage from "../Authentication/SignupPage";
 import LoginPage from "../Authentication/LoginPage";
 import CartPage from "../Cart/CartPage";
 import MyOrder from "../MyOrder/MyOrder";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Logout from "../Authentication/Logout";
+import ProtectedRoute from "./ProtectedRoute";
 
-const Routing = () => {
+const Routing = ({ user }) => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/products" element={<ProductsPage />} />
       <Route path="/product/:id" element={<SingleProductPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/cart" element={<CartPage />} />
-      <Route path="/myorders" element={<MyOrder />} />
-      <Route path="/logout" element={<Logout />} />
+      <Route
+        path="/signup"
+        element={user ? <Navigate to="/" /> : <SignupPage />}
+      />
+      <Route
+        path="/login"
+        element={user ? <Navigate to="/" /> : <LoginPage />}
+      />
+      <Route element={<ProtectedRoute user={user} />}>
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/myorders" element={<MyOrder />} />
+        <Route path="/logout" element={<Logout />} />
+      </Route>
     </Routes>
   );
 };
